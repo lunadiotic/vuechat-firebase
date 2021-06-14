f2668c18932e7596d60011d78ea3517b5b1d1663
lionatic <ahmadhakim404@gmail.com>
2021-06-14 14:55:15 +0700

    membuat fungsi show component login dan register

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="">
-    <Login />
-    <Register />
+    <div v-if="showLogin">
+      <Login />
+      <p>
+        Don't have an account?
+        <a href="#" @click="showLogin = false">Register here</a>
+      </p>
+    </div>
+    <div v-else>
+      <Register />
+      <p>
+        Already have an account?
+        <a href="#" @click="showLogin = true">Login here</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import Login from '../components/auth/Login'
 import Register from '../components/auth/Register'
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   components: {
     Login,
     Register,
+  },
+  setup() {
+    const showLogin = ref(false)
+
+    return { showLogin }
   },
 }
 </script>
