5248b1b0ada4e0f5e6ddc18b49aa7ce655b2053a
lionatic <ahmadhakim404@gmail.com>
2021-06-15 14:21:03 +0700

    membuat component chat room dan chat box

---
+++ src/components/chat/Form.vue
@@ -0,0 +1,16 @@
+<template>
+  <form action="" class="d-flex">
+    <input
+      type="text"
+      class="form-control rouned-0"
+      placeholder="Type your messages..."
+    />
+    <button class="btn btn-primary rounded-0">Send</button>
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/views/Room.vue
@@ -0,0 +1,27 @@
+<template>
+  <Navbar />
+  <main class="content mt-2 shadow-sm p-0">
+    <div class="bg-secondary text-white">
+      <h3 class="p-3">Chat Group</h3>
+    </div>
+    <div class="chat-wrapper">
+      <Chatbox />
+      <ChatForm />
+    </div>
+  </main>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import Chatbox from '../components/chat/Chatbox.vue'
+import ChatForm from '../components/chat/Form.vue'
+export default {
+  components: {
+    Navbar,
+    Chatbox,
+    ChatForm,
+  },
+}
+</script>
+
+<style></style>
