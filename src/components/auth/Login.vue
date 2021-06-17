1037a2de10ce87a52917a8200f7713dbd6fd0f13
lionatic <ahmadhakim404@gmail.com>
2021-06-17 23:35:11 +0700

    mengaktifkan fitur login dengan firebase

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,25 +1,43 @@
 <template>
-  <form>
-    <div class="alert alert-danger">
-      Error message!
+  <form @submit.prevent="handleSubmit">
+    <div class="alert alert-danger" v-if="error">
+      {{ error }}
     </div>
     <h3 class="h3 mb-3 fw-normal">Login</h3>
     <div class="form-floating">
-      <input type="email" class="form-control" />
+      <input type="email" class="form-control" v-model="email" />
       <label for="">Email Address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" />
+      <input type="password" class="form-control" v-model="password" />
       <label for="">Password</label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary">
+    <button class="w-100 btn btn-lg btn-primary" type="submit">
       Login
     </button>
   </form>
 </template>
 
 <script>
-export default {}
+import { ref } from 'vue'
+import useLogin from '../../composable/useLogin'
+
+export default {
+  setup() {
+    const email = ref('')
+    const password = ref('')
+    const { error, login } = useLogin()
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value)
+      if (!error.value) {
+        console.log('login success')
+      }
+    }
+
+    return { email, password, error, handleSubmit }
+  },
+}
 </script>
 
 <style></style>
