f1c1dffb2108b0dbc6d41a77b5745fe0bb4e5d5d
lionatic <ahmadhakim404@gmail.com>
2021-06-21 15:11:19 +0700

    mengubah format timestamp lebih mudah dibaca

--- src/components/chat/Chatbox.vue
+++ src/components/chat/Chatbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="documents" ref="chat" class="chat-box">
-    <div v-for="doc in documents" :key="doc.id" class="chat-bubble">
+    <div v-for="doc in formattedDocuments" :key="doc.id" class="chat-bubble">
       <div class="chat-head">
         <div>{{ doc.name }}</div>
         <div>{{ doc.createdAt }}</div>
@@ -14,17 +14,28 @@
 
 <script>
 import getCollection from '../../composable/getCollection'
-import { onUpdated, ref } from 'vue'
+import { computed, onUpdated, ref } from 'vue'
+import { formatDistanceToNow } from 'date-fns'
+
 export default {
   setup() {
     const { error, documents } = getCollection('message')
     const chat = ref(null)
 
+    const formattedDocuments = computed(() => {
+      if (documents.value) {
+        return documents.value.map((doc) => {
+          let time = formatDistanceToNow(doc.createdAt.toDate())
+          return { ...doc, createdAt: time }
+        })
+      }
+    })
+
     onUpdated(() => {
       chat.value.scrollTop = chat.value.scrollHeight
     })
 
-    return { error, documents, chat }
+    return { error, documents, chat, formattedDocuments }
   },
 }
 </script>
