f35efb1d03e46aee0ce3490f64185bd316c759e6
lionatic <ahmadhakim404@gmail.com>
2021-06-21 14:11:46 +0700

    menggunakan composable untuk mengirim data chat

--- src/components/chat/Form.vue
+++ src/components/chat/Form.vue
@@ -1,18 +1,45 @@
 <template>
-  <form action="" class="d-flex chat-form">
+  <form class="d-flex chat-form">
+    <div class="alert alert-danger">{{ error }}</div>
     <input
       type="text"
       class="form-control rouned-0"
       placeholder="Type your messages..."
+      required
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit"
     />
-    <button class="btn btn-primary rounded-0">
+    <button class="btn btn-primary rounded-0" @click.prevent="handleSubmit">
       <i class="fas fa-paper-plane"></i>
     </button>
   </form>
 </template>
 
 <script>
-export default {}
+import { ref } from 'vue'
+import useCollection from '../../composable/useCollection'
+import { timestamp } from '../../config/firebase'
+
+export default {
+  setup() {
+    const message = ref('')
+    const { error, storeDoc } = useCollection('message')
+
+    const handleSubmit = async () => {
+      const chat = {
+        name: 'Hakim',
+        message: message.value,
+        createdAt: timestamp(),
+      }
+      await storeDoc(chat)
+      if (!error.value) {
+        message.value = ''
+      }
+    }
+
+    return { message, handleSubmit, error }
+  },
+}
 </script>
 
 <style></style>
